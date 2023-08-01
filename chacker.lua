48c5e196d74c876e9e38f6496f46b99de7add969
WET HUB <126170177+WETHUBv1@users.noreply.github.com>
2023-08-01 23:44:14 +0700

    Update chacker.lua

--- chacker.lua
+++ chacker.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/WETHUBv1/Kumascripter/main/check.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/WETHUBv1/Kumascripter/main/Protected_1175261172459484.lua"))();
 else
     game.Players.LocalPlayer:Kick("Normal Hub :: Not Support This Game")
 end
